87c905846f84404cc848598fb3df4fb1d8b4d34a
niamtokik <mk@steepath.eu>
2017-08-26 09:53:37 +0200

    add metadata and words analysis

--- tools/parser.pl
+++ tools/parser.pl
@@ -7,10 +7,72 @@
 
   sub new {
     my $class = shift();
-    my $self = { @_ };
+    my $args = { @_ };
+    my $self = { };
     $self->{_counter} = 0;
+    $self->{_meta} = {};
+    $self->{_words} = {};
     $self->{_result} = [];
-    return bless($self, $class);
+    $self->{_meta}->{line} = $self->{_result};
+    $self->{_meta}->{words} = $self->{_words};
+    bless($self, $class);
+    $self->_meta($args);
+    return $self;
+  }
+
+  sub _words {
+    my $self = shift();
+    foreach my $word (@_) {
+      if ($self->{_words}->{$word}) { $self->{_words}->{$word} += 1 }
+      else { $self->{_words}->{$word} = 1 }
+    }
+  }
+
+  sub _meta {
+    my $self = shift();
+    my $args = shift();
+    if ($args->{author}) { $self->_author($args->{author}); }
+    if ($args->{origin}) { $self->_origin($args->{origin}); }
+    if ($args->{date})   { $self->_date($args->{date}); }
+    if ($args->{topic})  { $self->_topic($args->{topic}); }
+    if ($args->{title})  { $self->_title($args->{title}); }
+    if ($args->{source}) { $self->_source($args->{source}); }
+  }
+
+  sub _author {
+    my $self = shift();
+    my $args = shift();
+    $self->{_meta}->{author} = $args;
+  }
+
+  sub _origin {
+    my $self = shift();
+    my $origin = shift();
+    $self->{_meta}->{origin} = $origin;
+  }
+
+  sub _date {
+    my $self = shift();
+    my $date = shift();
+    $self->{_meta}->{date} = $date;
+  }
+
+  sub _topic {
+    my $self = shift();
+    my $topic = shift();
+    $self->{_meta}->{topic} = $topic;
+  }
+
+  sub _title {
+    my $self = shift();
+    my $title = shift();
+    $self->{_meta}->{topic} = $title;
+  }
+
+  sub _source {
+    my $self = shift();
+    my $sources = shift();
+    $self->{_meta}->{source} = $sources;
   }
 
   sub _result {
@@ -27,7 +89,7 @@
     chomp($line);
     my @split = split(/\s+/, $line);
     $self->_result("index", $self->{_counter});
-    $self->_result("date", $split[0]);
+    $self->_result("time", $split[0]);
     $self->router(\@split);
     $self->{_counter} += 1;
   }
@@ -48,8 +110,10 @@
       1;
     }
     if ($split[1] =~ m/^\</) {
-      $self->_result("author", join("", @split[1..2]));
+      $self->_result("pseudo", join("", @split[1..2]));
+      $self->_words(@split[3..($length-1)]);
       $self->_result("text", join(" ", @split[3..($length-1)]));
+      $self->_result("source", []);
     }
   }
 
@@ -62,15 +126,15 @@
     my $self = shift();
     foreach my $item (@{ $self->{_result} }) {
       printf("index: %d\n", $item->{index});
-      printf("date: %s\n", $item->{date});
-      printf("author: %s\n", $item->{author});
+      printf("time: %s\n", $item->{time});
+      printf("author: %s\n", $item->{pseudo});
       printf("data: %s\n", $item->{text});
     }
   }
 
   sub to_json {
     my $self = shift();
-    print encode_json $self->{_result};
+    print encode_json $self->{_meta};
   }
 
   1; }
@@ -79,7 +143,13 @@
   use strict;
   use warnings;
 
-  my $p = irc->new();
+  my $p = irc->new( origin => "" 
+                  , date => "today"
+                  , author => { "name" => "mulander" }
+                  , topic => "mytopic"
+                  , title => "mytitle"
+                  , source => [1,2,3]
+                  );
   foreach my $line (<>) {
     $p->parse($line);
   }
